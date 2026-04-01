b654399cc5ac2fcc77581c8b0144395ff2878208
juniorprotus <protuz2025@gmail.com>
2026-04-01 18:08:24 +0300

    Fix uuid module error - remove dependency

--- academia 2/backend/routes/assignments.js
+++ academia 2/backend/routes/assignments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { v4: uuidv4 } = require('uuid');
+
 const supabase = require('../utils/db');
 
 // Auth middleware (reuse from other routes)
